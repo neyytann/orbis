9f436eaafb8e40952ad5ece888e7c5779026f9c8
INTERN9-NVELASCO <intern9-nvelasco@INTERN9-NVELASCO.local>
2026-05-12 16:56:04 +0800

    full update

--- frontend/lib/widgets/Intern-TimeLog-Widgets/intern_time_log_filters.dart
+++ frontend/lib/widgets/Intern-TimeLog-Widgets/intern_time_log_filters.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../../theme/app_theme.dart';
-import '../../utils/responsive.dart';
 
 class InternTimeLogFilters extends StatelessWidget {
   final bool isDarkMode;
